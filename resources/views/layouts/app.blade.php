f045bf8bf47fd2a848b8e433eb3910c18081dd57
carlos martin <cjmar87@gmail.com>
2018-10-10 19:11:12 -0500

    sistemas de plantillas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://bootswatch.com/4/superhero/bootstrap.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
